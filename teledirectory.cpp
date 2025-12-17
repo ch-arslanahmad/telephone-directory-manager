45a5edb741f4cf2d96321b61075d40bad3dd5e6b
Muhammad Arslan <ch.arslanad@gmail.com>
2025-12-16 22:02:29 -0800

    Update teledirectory.cpp

--- teledirectory.cpp
+++ teledirectory.cpp
@@ -17,12 +17,64 @@ void printMenu() {
 }
 
 
+void add(map<string,string> & TD) {
+    string name, no;
+    
+    cout<<"\nEnter new Name: ";
+    getline(cin, name);
+
+    cout<<"\nEnter new No: ";
+    getline(cin, no);    
+    
+    auto state = TD.emplace(name,no);
+    
+    if(state.second) {
+        cout<<"Inserted Successfully";
+    }else {
+        cout<<"Unsuccessfull (duplicate).";
+    }
+    
+}
+
+
+void remove(map<string,string> & TD) {
+    string name, no;
+    
+    cout<<"\nEnter Name: ";
+    getline(cin, name);
+    if(TD.erase(name)) {
+        cout<<"Deleted Successfully";
+    }else {
+        cout<<"Unsuccessful (401).";
+    }
+}
+
+
+bool search(map<string,string> & TD) {
+    string name;
+    
+    cout<<"\nEnter new Name: ";
+    getline(cin, name);    
+    auto it = TD.find(name);
+    
+    if(it != TD.end()) {
+        cout<<"Record Exists."<<endl;
+        
+        cout<<"Name: "<<it->first;
+        cout<<"Phone Number: "<<it->second;
+    }
+}
+
+
 int main() {
+    
     
     // printMenu();
     
     // name, phone number (local code - number)
-    map<string,string> TD; // "Arslan", 042-3576115
+    map<string,string> TD {
+        {"Ayad", "042-25796115"},{"Bashir", "041-77665585"},{"Khalid", "021-66778000"}, {"Zubair", "051-667788"}
+    }; // "Arslan", 042-3576115
     
     
 }
